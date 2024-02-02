a29a3996b62d0a34f74ec2064715b6b7086e3eb9
Tanner Coker <tbcoker13@gmail.com>
2024-02-02 15:44:07 -0600

    Trying to setup analytics

--- gymweightcalculator/src/App.js
+++ gymweightcalculator/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header, BarbellCalculator, Footer } from './containers';
 import { Banner } from './components';
+import { inject } from '@vercel/analytics';
 
 import './App.css';
 
 const App = () => {
+  inject();
   return (
     <div className='app__bg app__wrapper'>
       <Banner title='Plate Calculator'/>
@@ -12,6 +14,7 @@ const App = () => {
       <BarbellCalculator />
       <Footer />
     </div>
+    
   )
 }
 
